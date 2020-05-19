8c0199ad9da5b126d3de9b425b48b159f7d0934d
tam <tam.sayers@teck-solutions.co.uk>
2020-05-19 11:46:28 +0100

    renaming settings for e2e plugin

--- src/main/scala/com/trueconnectivity/e2e/E2eTestPlugin.scala
+++ src/main/scala/com/trueconnectivity/e2e/E2eTestPlugin.scala
@@ -29,9 +29,9 @@ object E2eTestPlugin extends AutoPlugin {
         .getAbsolutePath,
       testCasesPackageTask := {
         HealthCheck.waitUntilHealthy(
-          dockerConnectPort.value,
-          dockerConnectRetryCount.value,
-          dockerConnectRetryDelayMs.value
+          e2eConnectPort.value,
+          e2eConnectRetryCount.value,
+          e2eConnectRetryDelayMs.value
         )
 
         (sbt.Keys.packageBin in IntegrationTest).value
@@ -49,8 +49,8 @@ object E2eTestPlugin extends AutoPlugin {
   }
 
   override lazy val projectSettings: Seq[Setting[_]] = Seq(
-    dockerConnectRetryCount := 10,
-    dockerConnectRetryDelayMs := 4000,
+    e2eConnectRetryCount := 10,
+    e2eConnectRetryDelayMs := 4000,
   ) ++ Defaults.itSettings ++ DockerComposePlugin.projectSettings ++ E2eConfig.settings
 
   override lazy val buildSettings: Seq[Setting[_]] = Seq()
